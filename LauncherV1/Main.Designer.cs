a0f0fd01c15c4829f2d2cb0390a24258fdffdbfc
Therealsxlar <104775507+Therealsxlar@users.noreply.github.com>
2024-05-10 22:16:57 -0400

    Init

---
+++ LauncherV1/Main.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace LauncherV1
+{
+    partial class Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Launcher));
+            this.LaunchButton = new Guna.UI2.WinForms.Guna2Button();
+            this.MainPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.ExitPictureBox = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.LauncherPictureBox = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.SettingsPictureBox = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.FortnitePathButton = new Guna.UI2.WinForms.Guna2Button();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ExitPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LauncherPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SettingsPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LaunchButton
+            // 
+            this.LaunchButton.BorderRadius = 5;
+            this.LaunchButton.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.LaunchButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LaunchButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.LaunchButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.LaunchButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.LaunchButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.LaunchButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(173)))), ((int)(((byte)(77)))));
+            this.LaunchButton.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaunchButton.ForeColor = System.Drawing.Color.White;
+            this.LaunchButton.Location = new System.Drawing.Point(16, 425);
+            this.LaunchButton.Name = "LaunchButton";
+            this.LaunchButton.Size = new System.Drawing.Size(342, 44);
+            this.LaunchButton.TabIndex = 0;
+            this.LaunchButton.Text = "Launch";
+            this.LaunchButton.Click += new System.EventHandler(this.LaunchButton_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(110)))), ((int)(((byte)(175)))));
+            this.MainPanel.BorderRadius = 5;
+            this.MainPanel.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.MainPanel.Controls.Add(this.ExitPictureBox);
+            this.MainPanel.Controls.Add(this.LauncherPictureBox);
+            this.MainPanel.Controls.Add(this.SettingsPictureBox);
+            this.MainPanel.Location = new System.Drawing.Point(-4, -1);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(375, 100);
+            this.MainPanel.TabIndex = 1;
+            // 
+            // ExitPictureBox
+            // 
+            this.ExitPictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(110)))), ((int)(((byte)(175)))));
+            this.ExitPictureBox.BorderRadius = 5;
+            this.ExitPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitPictureBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(24)))), ((int)(((byte)(39)))));
+            this.ExitPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("ExitPictureBox.Image")));
+            this.ExitPictureBox.ImageRotate = 0F;
+            this.ExitPictureBox.Location = new System.Drawing.Point(323, 9);
+            this.ExitPictureBox.Name = "ExitPictureBox";
+            this.ExitPictureBox.Size = new System.Drawing.Size(51, 33);
+            this.ExitPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ExitPictureBox.TabIndex = 63;
+            this.ExitPictureBox.TabStop = false;
+            this.ExitPictureBox.Click += new System.EventHandler(this.ExitPictureBox_Click);
+            // 
+            // LauncherPictureBox
+            // 
+            this.LauncherPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("LauncherPictureBox.Image")));
+            this.LauncherPictureBox.ImageRotate = 0F;
+            this.LauncherPictureBox.Location = new System.Drawing.Point(-53, -31);
+            this.LauncherPictureBox.Name = "LauncherPictureBox";
+            this.LauncherPictureBox.Size = new System.Drawing.Size(259, 133);
+            this.LauncherPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.LauncherPictureBox.TabIndex = 2;
+            this.LauncherPictureBox.TabStop = false;
+            // 
+            // SettingsPictureBox
+            // 
+            this.SettingsPictureBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(110)))), ((int)(((byte)(175)))));
+            this.SettingsPictureBox.BorderRadius = 5;
+            this.SettingsPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SettingsPictureBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(24)))), ((int)(((byte)(39)))));
+            this.SettingsPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("SettingsPictureBox.Image")));
+            this.SettingsPictureBox.ImageRotate = 0F;
+            this.SettingsPictureBox.Location = new System.Drawing.Point(278, 9);
+            this.SettingsPictureBox.Name = "SettingsPictureBox";
+            this.SettingsPictureBox.Size = new System.Drawing.Size(51, 33);
+            this.SettingsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SettingsPictureBox.TabIndex = 62;
+            this.SettingsPictureBox.TabStop = false;
+            // 
+            // FortnitePathButton
+            // 
+            this.FortnitePathButton.BorderRadius = 5;
+            this.FortnitePathButton.BorderStyle = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.FortnitePathButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FortnitePathButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.FortnitePathButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.FortnitePathButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.FortnitePathButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.FortnitePathButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(72)))), ((int)(((byte)(92)))));
+            this.FortnitePathButton.Font = new System.Drawing.Font("Arial Black", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FortnitePathButton.ForeColor = System.Drawing.Color.White;
+            this.FortnitePathButton.Location = new System.Drawing.Point(16, 378);
+            this.FortnitePathButton.Name = "FortnitePathButton";
+            this.FortnitePathButton.Size = new System.Drawing.Size(342, 41);
+            this.FortnitePathButton.TabIndex = 63;
+            this.FortnitePathButton.Text = "Select Fortnite Path";
+            // 
+            // Launcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(54)))), ((int)(((byte)(95)))));
+            this.ClientSize = new System.Drawing.Size(370, 481);
+            this.Controls.Add(this.FortnitePathButton);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.LaunchButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Launcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.Launcher_Load);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Launcher_MouseUp);
+            this.MainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ExitPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LauncherPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SettingsPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button LaunchButton;
+        private Guna.UI2.WinForms.Guna2Panel MainPanel;
+        private Guna.UI2.WinForms.Guna2PictureBox LauncherPictureBox;
+        private Guna.UI2.WinForms.Guna2PictureBox SettingsPictureBox;
+        private Guna.UI2.WinForms.Guna2Button FortnitePathButton;
+        private Guna.UI2.WinForms.Guna2PictureBox ExitPictureBox;
+    }
+}
+
---
+++ LauncherV1/Main.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LauncherV1
+{
+    public partial class Launcher : Form
+    {
+        private const int CORNER_RADIUS = 10; // the gui corner radius (can be changed)
+        private const int FADE_INTERVAL = 6;
+        private const double FADE_STEP = 0.03; // fade in speed (can be changed)
+        private double currentOpacity = 0.0;
+        private Timer fadeTimer;
+
+        private Point destination;
+        private bool isFadingIn = false;
+        private Point mouseOffset;
+        private bool isMouseDown = false;
+        private bool isFadingOut = false;
+
+        public Launcher()
+        {
+            InitializeComponent();
+            this.MouseDown += Launcher_MouseDown;
+            this.MouseMove += Launcher_MouseMove;
+            this.MouseUp += Launcher_MouseUp;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            fadeTimer = new Timer();
+            fadeTimer.Interval = FADE_INTERVAL;
+            fadeTimer.Tick += FadeTimer_Tick;
+            fadeTimer.Start();
+
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            destination = new Point((screenWidth - this.Width) / 2, (screenHeight - this.Height) / 2);
+
+            this.Location = new Point(screenWidth, (screenHeight - this.Height) / 2);
+        }
+
+        private void Launcher_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0;
+            isFadingIn = true; // if this is false the GUI will not load!!!!! DON'T CHANGE THIS
+        }
+
+
+        // Start on Fading in so on startup there's no lag
+        private void FadeTimer_Tick(object sender, EventArgs e)
+        {
+            if (isFadingIn)
+            {
+                currentOpacity += FADE_STEP;
+                if (currentOpacity >= 1.0)
+                {
+                    currentOpacity = 1.0;
+                    isFadingIn = false;
+                }
+
+                this.Opacity = currentOpacity;
+
+                int dx = (destination.X - this.Left) / 10;
+                int dy = (destination.Y - this.Top) / 10;
+                this.Location = new Point(this.Left + dx, this.Top + dy);
+            }
+            else if (isFadingOut)
+            {
+                currentOpacity -= FADE_STEP;
+                if (currentOpacity <= 0.0)
+                {
+                    currentOpacity = 0.0;
+                    isFadingOut = false;
+                    Environment.Exit(0);
+                }
+
+                this.Opacity = currentOpacity;
+            }
+        }
+
+        // Makes the GUI rounded DONT MESS WITH ANY OF THIS
+        private GraphicsPath GetRoundedRectangle(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(rect.X, rect.Y, radius * 2, radius * 2, 180, 90);
+            path.AddLine(rect.X + radius, rect.Y, rect.Right - radius * 2, rect.Y);
+            path.AddArc(rect.Right - radius * 2, rect.Y, radius * 2, radius * 2, 270, 90);
+            path.AddLine(rect.Right, rect.Y + radius * 2, rect.Right, rect.Bottom - radius * 2);
+            path.AddArc(rect.Right - radius * 2, rect.Bottom - radius * 2, radius * 2, radius * 2, 0, 90);
+            path.AddLine(rect.Right - radius * 2, rect.Bottom, rect.X + radius * 2, rect.Bottom);
+            path.AddArc(rect.X, rect.Bottom - radius * 2, radius * 2, radius * 2, 90, 90);
+            path.AddLine(rect.X, rect.Bottom - radius * 2, rect.X, rect.Y + radius * 2);
+            path.CloseFigure();
+            return path;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+            GraphicsPath path = GetRoundedRectangle(rect, CORNER_RADIUS);
+            this.Region = new Region(path);
+            using (Pen pen = new Pen(Color.FromArgb(0, 10, 10, 10), 2)) // transparent? - update IT'S TRANSPARENT GUYS!
+            {
+                e.Graphics.DrawPath(pen, path);
+            }
+        }
+        // End
+
+        // This is how the GUI moves when mouse is dragging the GUI yk?
+        private void Launcher_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = true;
+                mouseOffset = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Launcher_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point mousePos = Control.MousePosition;
+                mousePos.Offset(-mouseOffset.X, -mouseOffset.Y);
+                Location = mousePos;
+            }
+        }
+        private void Launcher_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDown = false;
+            }
+        }
+        // This is how the GUI moves when mouse is dragging the GUI yk?
+        // Ima also add it for the picture boxes so even if your trying to drag a picture box sense it's not the form it wont drag ykykykyk
+
+        private void ExitPictureBox_Click(object sender, EventArgs e)
+        {
+            isFadingOut = true;
+        }
+
+
+        // so legit is has no features yet!
+        private void LaunchButton_Click(object sender, EventArgs e)
+        {
+            string update = "attempting to update";
+            Console.WriteLine(update);
+        }
+    }
+}
